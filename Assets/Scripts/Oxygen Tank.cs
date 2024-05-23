0526cd72966af6517ea8098f012ed932d48c25a9
Jordan Oos <141826168+ZealousGem@users.noreply.github.com>
2024-05-23 21:57:17 +0200

    Cat Default Sprite and Oxygen UI Implemented

---
+++ Assets/Scripts/Oxygen Tank.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OxygenTank : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+  //  public GameObject Text;
+    public Image foreground;
+    public PlayerHealth health;
+    bool takehealth;
+    float timer;
+    float maxTime = 50f;
+    float damage = 1f;
+    void Start()
+    {
+        timer = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerHealth.airTime is true)
+        {
+            gainOxygen();
+        }
+
+        else
+        {
+            LosinigOxygen();
+        }
+
+       if(takehealth is true)
+        {
+            health.Takedamage(damage = Time.deltaTime);
+        }
+
+       
+      
+    }
+
+    void LosinigOxygen()
+    {
+        if (timer >= 0)
+        {
+            timer -= Time.deltaTime;
+            foreground.fillAmount = timer / maxTime;
+        }
+
+        else
+        {
+            foreground.fillAmount = 0;
+            takehealth = true;
+           
+
+        }
+    }
+
+    void gainOxygen()
+    {
+        takehealth = false;
+        if (timer <= 50)
+        {
+            timer += Time.deltaTime * 2;
+           // Debug.Log(timer);
+            foreground.fillAmount = timer / maxTime;
+        }
+
+        else
+        {
+            foreground.fillAmount = 50;
+        }
+    }
+}
+// https://www.youtube.com/watch?v=4g7YY9tLxEE&t=332s 
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
 
-    public int maxHealth = 100;
-    public int currentHealth;
+    public float maxHealth = 100f;
+    public float currentHealth;
+    private float HealthDisplay;
+    int health;
     public GameObject player;
+    public Text healthText;
+    public static bool airTime;
+   // public 
     
     
 
@@ -18,6 +24,8 @@ public class PlayerHealth : MonoBehaviour
         
         currentHealth = maxHealth;
         
+        
+        
        
         
 
@@ -26,20 +34,22 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        airTime = true;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
+        HealthDisplay = currentHealth;
+        health = (int)(HealthDisplay);
+        healthText.text = health.ToString();
+       // Debug.Log(currentHealth);
     }
 
-    public void Takedamage(int dam)
+    public void Takedamage(float dam)
     {
-        currentHealth -= dam;
+        currentHealth -=  dam;
         //Debug.Log($"{currentHealth}");
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);  // Ensure health stays within bounds
 
@@ -49,7 +59,7 @@ public class PlayerHealth : MonoBehaviour
             currentHealth = 0;
             
 
-            Destroy(gameObject);
+           // Destroy(gameObject);
             
 
 
@@ -58,5 +68,14 @@ public class PlayerHealth : MonoBehaviour
         }
 
     }
-    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Air": airTime = true; Debug.Log(airTime); break;
+            case "NoAir": airTime = false; Debug.Log(airTime); break;
+        }
+    }
+
 }
