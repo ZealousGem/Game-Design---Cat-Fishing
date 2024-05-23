46818a8654df9c31f65dde2ce8848448b58b92f2
Max Schulte <142296959+MaximusPrime29@users.noreply.github.com>
2024-05-23 10:50:21 +0200

    enemy moves toward player

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -4,70 +4,51 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private Rigidbody2D objRb;
-    float playerZ;
-    float playerX;
-    float playerY;  
-    GameObject[] player = null;
+    public float moveSpeed = 2f;
+    private Transform player;
+    
    
-     float EnemyX;
-    float EnemyY;
-    float speed;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        Physics2D.gravity = Vector2.zero;
-        player = GameObject.FindGameObjectsWithTag("Player");
-        objRb=GetComponent<Rigidbody2D>();
+        
+        
 
+    }
+    private void Awake()
+    {
+        GameObject[] playerObject = GameObject.FindGameObjectsWithTag("Player");
+        foreach (var pl in playerObject)
+        {
+            if (pl.GetComponent<Transform>())
+            {
+
+                player = pl.GetComponent<Transform>();
+                Debug.Log(player);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerZ = player[0].GetComponent<Transform>().position.z;
-        playerX = player[0].GetComponent<Transform>().position.x;
-        EnemyX=GetComponent<Transform>().position.x;
-        EnemyY = GetComponent<Transform>().position.y;
-        playerY= player[0].GetComponent<Transform>().position.y;
-        Vector2 movePos = Vector2.MoveTowards(objRb.position, new Vector2(playerX, playerY), speed * Time.fixedDeltaTime);
-        objRb.MovePosition(movePos);
-
-
-
-
-        if (EnemyX-playerX==3 || EnemyX - playerX == -3)
+        //the enemy move towards the player
+        if (player !=null)
         {
-            //Vector3 movepos = Vector3.MoveTowards(objRb.position, new Vector3(playerX, playerY, 0), speed * Time.fixedDeltaTime);
-            //Chase();
+            Vector2 direction =(player.position - transform.position).normalized;
+            transform.position=Vector2.MoveTowards(transform.position,player.position, moveSpeed *Time.deltaTime);
 
         }
-        if (EnemyY - playerY == 3 || EnemyY - playerY == -3)
-        {
-            //Chase();
+        
 
 
-        }
-        else
-        {
-            //Patrol();
 
-        }
+
+       
 
 
     }
-    private void Chase()
-    {
-        Debug.Log("should be chasing");
-        Vector2 movePos = Vector2.MoveTowards(objRb.position, new Vector2(playerX, playerY), speed * Time.fixedDeltaTime);
-        objRb.MovePosition(movePos);
-
-
-    }
-    private void Patrol()
-    {
-        transform.position += new Vector3(1, 0, 0) * Time.deltaTime; ;
-
-    }
+   
 }
